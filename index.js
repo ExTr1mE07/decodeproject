2e9cc4cb9c25376774f5f70847b02cb102787355
ExTr1mE07 <t.alpyspayevgg@gmail.com>
2023-09-15 19:18:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express  = require('express');
+const app = express();
+
+app.use(express.static('public'))
+app.set('view engine' , 'ejs');
+
+app.listen(3000, () => {
+    console.log('server work on port 3000')
+})
+
+app.get ('/', (req, res) => {
+    res.end('Hello')
+})
+app.get ('/header', (req, res) => {
+    res.render('header.ejs')
+})
+
+
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server work on port ${PORT}`)
+// })
